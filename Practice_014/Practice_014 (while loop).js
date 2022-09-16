cce3ee3219ffc1ba220706e723021e7a9c9cb84e
Roozbeh Lazemzadeh <rz.lazem@gmail.com>
2022-09-16 10:14:10 +0430

    while loop added to Practice_014

---
+++ Practice_014/Practice_014 (while loop).js
@@ -0,0 +1,43 @@
+// for -----------------------------------------
+// const WannaPlay = confirm("Do you want to play?");
+
+// if (WannaPlay) {
+// 	const randomNumber = Math.floor(Math.random() * 10) + 1;
+// 	console.log(randomNumber);
+// 	let guess = prompt("Guess the number between 0 to 10?");
+// 	if (guess == randomNumber) {
+// 		alert(`Good job! you guessed ${guess} with your first try! `);
+// 	} else {
+// 		for (let i = 1; i < 5; i++) {
+// 			guess = prompt(`Guess another one, you have ${5 - i} chances left! `);
+// 			if (guess == randomNumber) {
+// 				alert(`Good job, the number was ${randomNumber} `);
+// 				break;
+// 			}
+// 			if (5 - i <= 1) {
+// 				alert(`you lost! the number was ${randomNumber}`);
+// 			}
+// 		}
+// 	}
+// }
+
+// While -----------------------------------------
+
+const WannaPlay = confirm("Do you want to play?");
+
+if (WannaPlay) {
+	const randomNumber = Math.floor(Math.random() * 10) + 1;
+	console.log(randomNumber);
+	let chances = 5;
+	while (chances > 0) {
+		let guess = prompt(`Guess the number between 0 to 10 (${chances} try!)?`);
+		chances--;
+		if (guess == randomNumber) {
+			alert(`Good job, the number was ${randomNumber} `);
+			break;
+		}
+		if (chances == 0 && guess != randomNumber) {
+			alert(`you lost! the number was ${randomNumber}`);
+		}
+	}
+}
